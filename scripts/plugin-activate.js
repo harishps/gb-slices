535821af18c809c025dcaed41253daff439a42da
gopinathnatarajan <48278541+gopinathnatarajan@users.noreply.github.com>
2021-09-07 23:03:49 +0530

    css updated

---
+++ scripts/plugin-activate.js
@@ -0,0 +1,9 @@
+// jQuery document ready
+$(document).ready(function() {
+  $(".my-rating").starRating({
+    initialRating: 4,
+    strokeColor: '#894A00',
+    strokeWidth: 10,
+    starSize: 25
+  });
+});
